a9fc1366e095b2c9718c7f550edad50ebe649f02
Vasew <vasiliykolesov@yahoo.com.au>
2023-09-13 12:47:58 +0300

    Test js 1.5

--- lib/src/utils/html_utils.dart
+++ lib/src/utils/html_utils.dart
@@ -298,12 +298,10 @@ class HtmlUtils {
         }
       });
 
+      setTimeout(function(){
       console.log("WTF start! " + frameElement + " - " + document.links.length + " ?");
-      if (frameElement && document.links[0] && document.links[0].href) {
-        console.log("WTF is load?");
-        var returnedObject = JSON.stringify({method: 'get', href: document.links[0].href});
-        frameElement.contentWindow.$webOnLoadIframeCallback && frameElement.contentWindow.$webOnLoadIframeCallback(returnedObject)
-      }
+
+      },5000);
 
 
       document.addEventListener('load', function() {
