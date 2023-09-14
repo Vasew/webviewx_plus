279279669d81424291085f21dc60139a1584bee5
Vasew <vasiliykolesov@yahoo.com.au>
2023-09-14 10:09:23 +0300

    Test js 3.3

--- lib/src/utils/html_utils.dart
+++ lib/src/utils/html_utils.dart
@@ -275,19 +275,19 @@ class HtmlUtils {
         }
       });
 
-      var interval = window.setInterval(function(){
-        var isComplete = "$pageUrl".indexOf('/complete_3ds') > 0;
-        console.log("isComplete: " + document.links.length + " - " + frameElement + "");
-        if (frameElement && document.links.length > 0) {
-          console.log("interval in");
-          var returnedObject = JSON.stringify({method: 'get', href: document.links[0].href});
-          frameElement.contentWindow.$webOnLoadIframeCallback && frameElement.contentWindow.$webOnLoadIframeCallback(returnedObject);
+      if("$pageUrl".indexOf('/complete_3ds') > 0){
+        var interval = window.setInterval(function(){
+          if (frameElement && document.links.length > 0 ) {
+            console.log("interval in");
+            var returnedObject = JSON.stringify({method: 'get', href: document.links[0].href});
+            frameElement.contentWindow.$webOnLoadIframeCallback && frameElement.contentWindow.$webOnLoadIframeCallback(returnedObject);
+            clearInterval(interval);
+          }
+        },100);
+        setTimeout(()=>{
           clearInterval(interval);
-        }
-      },100);
-      setTimeout(()=>{
-        clearInterval(interval);
-      }, 5000);
+        }, 5000);
+      }
 
       document.addEventListener('submit', e => {
         if (frameElement && document.activeElement && document.activeElement.form && document.activeElement.form.action) {
